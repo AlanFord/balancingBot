5846c169deee9a12d85962cc2d101c661c25b2e3
Alan Ford <alanford354@gmail.com>
2020-07-05 08:52:46 -0400

    corrected the battery voltage calculation

--- firmware/Balancing_robot/misc.ino
+++ firmware/Balancing_robot/misc.ino
@@ -22,15 +22,19 @@ void initialize_timer() {
 bool battery_voltage_is_low() {
   int voltage;
   //Load the battery voltage to the voltage variable.
-  //850 is the voltage compensation for the diode.
-  //Resistor voltage divider => (3.3k + 2.15k)/2.15k = 2.5349
-  //12.674V equals ~5V @ Analog 0.
-  //12.472V equals 4.92V @ Analog 0.
-  //12472 / 1023 =
-  //12.674V equals 1023 analogRead(0).
-  //12674 / 1023 = 12.192.
+  //850 mV is the voltage compensation for the diode.
+  //Resistor voltage divider => 21.5k/(33k + 21.5k) = 1/2.535
+  //Arduino analog input A0 reads 0 at 0V and 1023 at 5V, 
+  // or 0.00489V/division
+  // so,
+  //voltage divider voltage = A0 * 0.00489V/division
+  //battery voltage = (A0 * 0.00489V/division)*2.535 + 0.85V
+  // or
+  //battery voltage = (A0 * 0.0124) + 0.85V
+  // in millivolts (integers make things easier)
+  // battery mV = (A0 *12.4) + 850
   //The variable voltage holds 1050 if the battery voltage is 10.5V.
-  voltage = (analogRead(0) * 12.191) + 850;
+  voltage = (analogRead(0) * 12.4) + 850;
   if (voltage < 10500 && voltage > 8000) { //If batteryvoltage is below 10.5V and higher than 8.0V
     return true;
   }
